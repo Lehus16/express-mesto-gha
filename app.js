6f27c0346bb881b6cf04454dd3f3b495bd98b7c7
Alexey Ushakov <alexeyushakov16@gmail.com>
2023-10-18 09:41:44 +0300

    проверка

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-unresolved */
+require('dotenv').config();
 const express = require('express');
-// eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const centralizedErrorHandler = require('./middlewares/errorHandler');
 const router = require('./routes/index');
-const Statuses = require('./utils/codeStatus');
+const NotFoundError = require('./errors/notFound');
 
 const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
 const app = express();
 app.use(express.json());
-
-app.use((req, res, next) => {
-  req.user = {
-    _id: '651a9f496c1c265b24b75715',
-  };
-  next();
-});
+app.use(cookieParser());
 
 app.use(router);
-app.use('*', (req, res) => res.status(Statuses.NOT_FOUND).send({ message: 'Запрашиваемый ресурс не найден' }));
+app.use('*', (req, res, next) => next(new NotFoundError('Запрашиваемый ресурс не найден')));
+
+app.use(errors());
+app.use(centralizedErrorHandler);
 
 async function init() {
   await mongoose.connect(MONGO_URL);
   await app.listen(PORT);
-
   // eslint-disable-next-line no-console
-  console.log(`app listening on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 }
 
 init();
