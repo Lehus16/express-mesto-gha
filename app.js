d5a1ca2d110181574c6d02854d556d7d9cbd92d7
Alexey Ushakov <alexeyushakov16@gmail.com>
2024-01-14 14:44:23 +0300

    some

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { errors } = require('celebrate');
 const centralizedErrorHandler = require('./middlewares.js/errorHandler');
 const router = require('./routes/index');
 const NotFoundError = require('./errors/notFound');
+const { requestLogger, errorLogger } = require('./middlewares.js/logger');
 
 const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
@@ -14,10 +15,19 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+// Логгер запросов
+app.use(requestLogger);
+
+// Обработчик роутов
 app.use(router);
+
+// Логгер ошибок
+app.use(errorLogger);
+
 app.use('*', (req, res, next) => next(new NotFoundError('Запрашиваемый ресурс не найден')));
 
 app.use(errors());
+// Централизованный обработчик ошибок
 app.use(centralizedErrorHandler);
 
 async function init() {
